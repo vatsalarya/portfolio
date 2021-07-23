703a20199231c4cead776b3000553b6861d3b38c
Vatsal Arya <vatsalarya1@gmail.com>
2021-07-23 08:02:48 +0530

    Modified sidemenu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,13 @@
 import "./menu.scss";
 import HomeIcon from '@material-ui/icons/Home';
 import Brightness4Icon from '@material-ui/icons/Brightness4';
+import WorkIcon from '@material-ui/icons/Work';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PeopleIcon from '@material-ui/icons/People';
+import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import CodeIcon from '@material-ui/icons/Code';
 
 export default function Menu({ menuOpen, setMenuOpen }) {
   return (
@@ -10,39 +17,38 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <Brightness4Icon className="icon" />
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <HomeIcon className="icon" />
+          <HomeIcon className="icon homeicon" />
           <a href="#intro">Home</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <HomeIcon className="icon" />
+          <WorkIcon className="icon" />
           <a href="#portfolio">Portfolio</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <HomeIcon className="icon" />
+          <ListAltIcon className="icon" />
           <a href="#works">Works</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <HomeIcon className="icon" />
+          <PeopleIcon className="icon" />
           <a href="#testimonials">Testimonials</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <HomeIcon className="icon" />
+          <ContactPhoneIcon className="icon" />
           <a href="#contact">Contact</a>
         </li>
         <li className="bottom" onClick={()=>setMenuOpen(false)}>
-          <HomeIcon className="icon" />
-          <a href="#contact">LinkedIn</a>
+          <LinkedInIcon className="icon" />
+          <a href="https://www.linkedin.com/in/vatsal-arya/">LinkedIn</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <HomeIcon className="icon" />
-          <a href="#contact">Twitter</a>
+          <GitHubIcon className="icon" />
+          <a href="https://github.com/vatsalarya">GitHub</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <HomeIcon className="icon" />
-          <a href="#contact">GitHub</a>
+          <CodeIcon className="icon" />
+          <a href="https://www.codechef.com/users/vatsalarya1">CodeChef</a>
         </li>
-       
-        
+
       </ul>
     </div>
   );
